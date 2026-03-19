af82a3f5809cfd5a15f0e6d755cc5bba8c8f393f
Mkowal <wrx93174@student.wroclaw.merito.pl>
2026-03-20 00:19:55 +0100

    interface WIP

---
+++ inc/interface.h
@@ -0,0 +1,14 @@
+#include<iostream>
+
+
+template<class>
+class interface {
+    virtual container;
+    public:
+        virtual void pushBack(){
+            std::cout<<"Dodaj elem"<<std::endl;
+        }
+};
+
+
+
